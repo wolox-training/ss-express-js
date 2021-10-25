2f5d827db1693f686b9378edc411eac88b61be74
sebastian <sebastian.spadea@wolox.com.ar>
2021-10-25 11:18:20 -0300

    added requested changes

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
+const config = require('../../config/index');
 
 exports.requestPhrases = axios.create({
-  baseURL: process.env.PHRASES_URL,
+  baseURL: config.common.api.baseURL,
   timeout: 1000,
   headers: {
     accept: 'application/json'
-  }
+  },
+  method: 'get'
 });
