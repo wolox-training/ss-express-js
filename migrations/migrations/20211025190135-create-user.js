dbfaa8405e3cae212029c00ad6ef892a7f58f349
sebastian <sebastian.spadea@wolox.com.ar>
2021-10-26 11:52:29 -0300

    Little changes

--- migrations/migrations/20211025190135-create-user.js
+++ migrations/migrations/20211025190135-create-user.js
@@ -35,5 +35,5 @@ module.exports = {
         type: Sequelize.DATE
       }
     }),
-  down: queryInterface => queryInterface.dropTable('Users')
+  down: queryInterface => queryInterface.dropTable('users')
 };
