7ea9f11050e7d75cd0d5b6b604d2352af6cff0bb
sebastian <sebastian.spadea@wolox.com.ar>
2021-11-02 12:08:42 -0300

    requested changes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,7 +1,8 @@
 const { createUser } = require('../services/user');
 const asyncWrapper = require('../utils/asycWrapper');
+const { statusCodes } = require('../constants/codes');
 
 exports.newUser = asyncWrapper(async (req, res) => {
   const user = await createUser(req.body);
-  return res.status(201).json({ first_name: user.firstName });
+  return res.status(statusCodes.CREATED).json({ first_name: user.firstName });
 });
--- app/controllers/weets.js
+++ app/controllers/weets.js
@@ -1,7 +1,8 @@
 const getPhrases = require('../services/weet');
 const asyncWrapper = require('../utils/asycWrapper');
+const { statusCodes } = require('../constants/codes');
 
 exports.getWeets = asyncWrapper(async (req, res) => {
   const phrases = await getPhrases();
-  return res.status(200).json(phrases);
+  return res.status(statusCodes.OK).json(phrases);
 });
